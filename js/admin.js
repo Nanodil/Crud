a3853ba20b20f5f3f667a27f9db11de8cec6fcec
nano <marianodilella86@gmail.com>
2022-03-31 15:41:23 -0300

    editar producto formulario

--- js/admin.js
+++ js/admin.js
@@ -10,7 +10,7 @@ import { Producto } from "./productoClass.js";
 
 //declarar variables
 let listaProductos = [];
-
+let productoExistente = false; //false tengo que agregar un producto nuevo, true tengo que modificar
 //Este archivo tendra toda a logica de ABM o CRUD
 let producto = document.querySelector("#producto");
 let cantidad = document.querySelector("#cantidad");
@@ -18,6 +18,7 @@ let codigo = document.querySelector("#codigo");
 let descripcion = document.querySelector("#descripcion");
 let url = document.querySelector("#url");
 let formulario = document.querySelector("#formProducto");
+let btnAgregar = document.querySelector("#btnAgregar");
 
 producto.addEventListener("blur", () => {
   validarCampoRequerido(producto);
@@ -36,6 +37,7 @@ url.addEventListener("blur", () => {
 });
 formulario.addEventListener("submit", guardarProducto);
 
+btnAgregar.addEventListener('click', limpiarFormulario)
 //verificar si hay datos en localStorage
 cargaInicial();
 
@@ -43,10 +45,17 @@ function guardarProducto(event) {
   event.preventDefault();
   // validar datos del formulario
   if (validarGeneral()) {
-    // crear un nuevo producto
-    console.log("aqui deberia crear un producto");
-
-    agregarProducto();
+    //tengo que modificar o tengo que modificar algo nuevo
+    //if(productoExistente)
+    if (productoExistente) {
+      //modificar
+      actualizarProducto();
+    } else {
+      //agregar
+      // crear un nuevo producto
+      console.log("aqui deberia crear un producto");
+      agregarProducto();
+    }
   } else {
     console.log("aqui solo mostrar el cartel de error");
   }
@@ -78,10 +87,9 @@ function cargaInicial() {
 
   console.log("listaProductos");
   //llamar a la funcion que crea fila
-  listaProductos.forEach(itemProducto =>{
+  listaProductos.forEach((itemProducto) => {
     crearFila(itemProducto);
-
-  })
+  });
 }
 
 function crearFila(itemProducto) {
@@ -96,15 +104,71 @@ function crearFila(itemProducto) {
   <td>${itemProducto.cantidad}</td>
   <td>${itemProducto.url}</td>
   <td>
-    <button class="btn btn-warning">Editar</button>
+    <button class="btn btn-warning" onclick="prepararEdicionProducto('${itemProducto.codigo}')">Editar</button>
     <button class="btn btn-danger">Borrar</button>
   </td>
 </tr>`;
 }
 
-function limpiarFormulario(){
+function limpiarFormulario() {
   //limpia los value de los elementos del form
   formulario.reset();
   //limpiar las clases de cada elemento del form
-  codigo.className = 'form-control';
+  codigo.className = "form-control";
+productoExistente = false;
+}
+
+//funcion invocada desde el html
+window.prepararEdicionProducto = (codigo) => {
+  //console.log(codigo);
+  //buscar el objeto dentro del arreglo
+  let productoEncontrado = listaProductos.find((itemProducto) => {
+    return itemProducto.codigo == codigo;
+  });
+  //console.log(productoEncontrado);
+  //mostrar los datos del objeto en formulario
+  document.querySelector("#codigo").value = productoEncontrado.codigo;
+  document.querySelector("#producto").value = productoEncontrado.nombreProducto;
+  document.querySelector("#descripcion").value = productoEncontrado.descripcion;
+  document.querySelector("#cantidad").value = productoEncontrado.cantidad;
+  document.querySelector("#url").value = productoEncontrado.url;
+
+  //cambiar el valor de la variable bandera para editar
+  productoExistente = true;
+};
+
+function actualizarProducto() {
+  console.log("aqui tengo que modificar producto");
+  console.log(codigo.value);
+
+  //buscar la posicion del objeto con el codigo indicado
+  let indiceProducto = listaProductos.findIndex((itemProducto) => {
+    return itemProducto.codigo == codigo.value;
+  });
+
+  //actualizar los valores del objeto encontrado dentro de mi arreglo
+  listaProductos[indiceProducto].nombreProducto =
+    document.querySelector("#producto").value;
+  listaProductos[indiceProducto].descripcion =
+    document.querySelector("#descripcion").value;
+  listaProductos[indiceProducto].cantidad =
+    document.querySelector("#cantidad").value;
+  listaProductos[indiceProducto].url = document.querySelector("#url").value;
+
+  console.log(listaProductos[indiceProducto]);
+
+  //actualizar el localStorage
+  localStorage.setItem("listaProductosKey", JSON.stringify(listaProductos));
+
+  //actualizar la tabla
+  borrarFilas();
+  listaProductos.forEach((itemProducto) => {
+    crearFila(itemProducto);
+  });
+}
+
+function borrarFilas() {
+  //traigo el nodo padre
+  let tabla = document.querySelector("#tablaProductos");
+  tabla.innerHTML = "";
 }
