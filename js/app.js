7ffcd93eab962022bc9f46f15d524e73d1c3a427
nano <marianodilella86@gmail.com>
2022-04-05 10:32:05 -0300

    se agrego el maquetado en la pagina y logica en la pagina principal

--- js/app.js
+++ js/app.js
@@ -2,17 +2,30 @@
 let productos = [];
 
 cargaInicial();
-function cargaInicial(){
-    productos = JSON.parse(localStorage.getItem('listaProductosKey')) || [];
+function cargaInicial() {
+  productos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
 
-    //si hay datos dentro del arreglo dibujo las columnas con cards
-    if(productos.length >0){
-        //aqui dibujo las cards
-        crearColumna();
-    }
+  //si hay datos dentro del arreglo dibujo las columnas con cards
+  if (productos.length > 0) {
+    //aqui dibujo las cards
+    productos.forEach((producto) => {
+      crearColumna(producto);
+    });
+  }
 }
 
-function crearColumna(){
-    let grilla = document.querySelector('#grilla');
-    console.log(grilla);
+function crearColumna(producto) {
+  let grilla = document.querySelector("#grilla");
+  console.log(grilla);
+  grilla.innerHTML += ` <div class="col-sm-12 col-md-4 col-lg-3 mb-3">
+
+    <div class="card" >
+      <img src="${producto.url}" class="card-img-top" alt="${producto.nombreProducto}">
+      <div class="card-body">
+        <h5 class="card-title">${producto.nombreProducto}</h5>
+        <p class="card-text">${producto.descripcion}</p>
+      </div>
+
+    </div>
+  </div>`;
 }
